c2b788bcc848c4cca616bc33e0a62cc4896c7dd2
santosh <ptnnsantosh@gmail.com>
2024-03-19 10:09:12 +0530

    Added test case

--- src/test/java/com/springBoot/jenkins/SpringBootGitJenkinsApplicationTests.java
+++ src/test/java/com/springBoot/jenkins/SpringBootGitJenkinsApplicationTests.java
@@ -1,13 +1,21 @@
 package com.springBoot.jenkins;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootGitJenkinsApplicationTests {
-
+	
+	public static Logger log = LoggerFactory.getLogger(SpringBootGitJenkinsApplication.class);
+	
 	@Test
 	void contextLoads() {
+		log.info("With in test case...");
+		assertEquals(true, true);
 	}
 
 }
